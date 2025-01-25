fb8e10cea42ec83daf83a09f9d7c776cb3ab7908
Michael lemito <delovoy.lemit@gmail.com>
2025-01-25 16:22:25 +0300

    возможно тут появится паттерн builder

---
+++ GenerativePatterns/Builder/Builder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lemito on 1/25/25.
+//
+
+#include "Builder.h"
---
+++ GenerativePatterns/Builder/Builder.h
@@ -0,0 +1,16 @@
+//
+// Created by lemito on 1/25/25.
+//
+
+#ifndef BUILDER_H
+#define BUILDER_H
+
+
+
+class Builder {
+
+};
+
+
+
+#endif //BUILDER_H
