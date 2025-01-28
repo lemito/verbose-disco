13d90c32a84abf8424c9ee25d165c5cf2efd7ccd
Michael lemito <delovoy.lemit@gmail.com>
2025-01-28 11:41:45 +0300

    мяу

---
+++ BehavioralPatterns/Iterators/BST/BST.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lemito on 1/28/25.
+//
+
+#include "BST.h"
---
+++ BehavioralPatterns/Iterators/BST/BST.h
@@ -0,0 +1,16 @@
+//
+// Created by lemito on 1/28/25.
+//
+
+#ifndef BST_H
+#define BST_H
+
+
+
+class BST {
+
+};
+
+
+
+#endif //BST_H
